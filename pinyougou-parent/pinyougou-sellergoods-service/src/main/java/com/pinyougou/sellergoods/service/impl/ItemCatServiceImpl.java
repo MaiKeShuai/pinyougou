b0cf69bf7455fadf5b16ce8addac39ad3f91d627
maikebai <2467881217@qq.com>
2019-06-29 16:59:04 +0800

    面包屑完成

---
+++ pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,111 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import java.util.List;
+
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbItemCatMapper;
+import com.pinyougou.pojo.TbItemCat;
+import com.pinyougou.pojo.TbItemCatExample;
+import com.pinyougou.pojo.TbItemCatExample.Criteria;
+import com.pinyougou.sellergoods.service.ItemCatService;
+
+
+/**
+ * 服务实现层
+ *
+ * @author Administrator
+ */
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
+
+    /**
+     * 查询全部
+     */
+    @Override
+    public List<TbItemCat> findAll() {
+        return itemCatMapper.selectByExample(null);
+    }
+
+    /**
+     * 按分页查询
+     */
+    @Override
+    public PageInfo<TbItemCat> findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<TbItemCat> list = itemCatMapper.selectByExample(null);
+        return new PageInfo<TbItemCat>(list);
+    }
+
+    /**
+     * 增加
+     */
+    @Override
+    public void add(TbItemCat itemCat) {
+        itemCatMapper.insert(itemCat);
+    }
+
+
+    /**
+     * 修改
+     */
+    @Override
+    public void update(TbItemCat itemCat) {
+        itemCatMapper.updateByPrimaryKey(itemCat);
+    }
+
+    /**
+     * 根据ID获取实体
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public TbItemCat findOne(Long id) {
+        return itemCatMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            itemCatMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+
+    @Override
+    public PageInfo<TbItemCat> findPage(TbItemCat itemCat, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+
+        TbItemCatExample example = new TbItemCatExample();
+        Criteria criteria = example.createCriteria();
+
+        if (itemCat != null) {
+            if (itemCat.getName() != null && itemCat.getName().length() > 0) {
+                criteria.andNameLike("%" + itemCat.getName() + "%");
+            }
+
+        }
+
+        List<TbItemCat> list = itemCatMapper.selectByExample(example);
+        return new PageInfo<TbItemCat>(list);
+    }
+
+    @Override
+    public List<TbItemCat> findByParentId(Long parentId) {
+
+        TbItemCatExample exampl = new TbItemCatExample();
+        Criteria criteria = exampl.createCriteria();
+        criteria.andParentIdEqualTo(parentId);
+        List<TbItemCat> list = itemCatMapper.selectByExample(exampl);
+        return list;
+    }
+}
